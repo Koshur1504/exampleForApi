fecc97513e8936e32018b2ee519848a0aba30f1b
Koshur1504 <aamir.mir530@gmail.com>
2024-03-25 06:31:04 +0530

    sdfgh

--- api/server.js
+++ api/server.js
@@ -3,7 +3,16 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const auth = require("json-server-auth");
 const cors = require("cors");
-const router = jsonServer.router("db.json");
+// const router = jsonServer.router("db.json");
+
+
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
 
 const middlewares = jsonServer.defaults();
 server.db = router.db;
