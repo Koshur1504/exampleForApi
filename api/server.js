29e9a9a652818c2c6748f7fdbacc6db16bdd9e51
Koshur1504 <aamir.mir530@gmail.com>
2024-03-25 06:34:57 +0530

    asdfg

--- api/server.js
+++ api/server.js
@@ -3,28 +3,17 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const auth = require("json-server-auth");
 const cors = require("cors");
-// const router = jsonServer.router("db.json");
-
-
 const fs = require('fs')
 const path = require('path')
 const filePath = path.join('db.json')
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db)
-
-
 const middlewares = jsonServer.defaults();
 server.db = router.db;
 
 server.use(middlewares);
-// Add this before server.use(router)
-// server.use(
-//   jsonServer.rewriter({
-//     "/api/*": "/$1",
-//     "/blog/:resource/:id/show": "/:resource/:id",
-//   })
-// );
+
 
 server.use(cors());
 server.use(auth);
@@ -37,11 +26,8 @@ server.listen(port, () => {
   console.log(`JSON Server with authentication is running on port ${port}`);
 });
 
-// Export the Server API
+
 module.exports = server;
 
-
-
-// Set up the authentication middleware
 
 
