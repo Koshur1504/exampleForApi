4e07c417e4b3d101884f4c34717dfb13111a8705
Koshur1504 <aamir.mir530@gmail.com>
2024-03-25 06:15:11 +0530

    ds

--- api/server.js
+++ api/server.js
@@ -1,21 +1,38 @@
-const jsonServer = require('json-server')
+const jsonServer = require("json-server");
 
-const server = jsonServer.create()
+const server = jsonServer.create();
+const auth = require("json-server-auth");
+const cors = require("cors");
+const router = jsonServer.router("db.json");
 
-const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults();
+server.db = router.db;
 
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
+server.use(middlewares);
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+
+server.use(cors());
+server.use(auth);
+server.use(middlewares);
+server.use(router);
+
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log(`JSON Server with authentication is running on port ${port}`);
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
+
+
+
+// Set up the authentication middleware
+
+
